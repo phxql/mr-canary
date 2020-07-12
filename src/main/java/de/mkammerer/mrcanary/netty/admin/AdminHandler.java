15a7133c7255dca7af4028a9e73b9b94115a29d0
Moritz Kammerer <kammerer.moritz@gmail.com>
2020-07-12 17:54:13 +0200

    Implement /status on the admin api

--- src/main/java/de/mkammerer/mrcanary/netty/admin/AdminHandler.java
+++ src/main/java/de/mkammerer/mrcanary/netty/admin/AdminHandler.java
@@ -1,18 +1,25 @@
 package de.mkammerer.mrcanary.netty.admin;
 
+import com.google.gson.Gson;
 import de.mkammerer.mrcanary.netty.NettyHelper;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import io.netty.handler.codec.http.HttpVersion;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
 @Slf4j
 public class AdminHandler extends ChannelInboundHandlerAdapter {
+    private final Gson gson = new Gson();
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         if (msg instanceof HttpRequest) {
@@ -21,11 +28,33 @@ public class AdminHandler extends ChannelInboundHandlerAdapter {
     }
 
     private void handle(ChannelHandlerContext ctx, HttpRequest request) {
-        FullHttpResponse response = new DefaultFullHttpResponse(
-            HttpVersion.HTTP_1_0, HttpResponseStatus.OK, Unpooled.EMPTY_BUFFER
-        );
+        FullHttpResponse response;
+
+        switch (request.uri()) {
+            case "/status":
+                response = json(handleStatus());
+                break;
+            default:
+                response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_0, HttpResponseStatus.NOT_FOUND);
+                break;
+        }
+
         ctx.write(response);
         NettyHelper.flushAndClose(ctx.channel());
+    }
+
+    private FullHttpResponse json(Object message) {
+        String json = gson.toJson(message);
+
+        DefaultFullHttpResponse response = new DefaultFullHttpResponse(
+            HttpVersion.HTTP_1_0, HttpResponseStatus.OK, Unpooled.copiedBuffer(json, StandardCharsets.UTF_8)
+        );
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "application/json");
+        return response;
+    }
+
+    private Object handleStatus() {
+        return Map.of("status", "ok");
     }
 
     @Override
