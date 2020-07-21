245f8a05f404db49892321b73a1305d5c9ccdf8d
Moritz Kammerer <kammerer.moritz@gmail.com>
2020-07-21 20:04:25 +0200

    Increase load testing interval

--- test/start-load.sh
+++ test/start-load.sh
@@ -2,4 +2,4 @@
 
 # Uses https://github.com/giltene/wrk2
 
-wrk --connections 1 --duration 1m --threads 1 --rate 2 --header "Connection: close" http://localhost:8080/
+wrk --connections 1 --duration 1h --threads 1 --rate 2 --header "Connection: close" http://localhost:8080/
